0f37e9bea5d990488abd656bdc33b03937822987
Максим Груздев <maestrosuper@mail.ru>
2024-01-20 19:08:59 +0300

    Добавьте файлы проекта.

---
+++ Match.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadingExampleApp
+{
+    internal class Match
+    {
+        private readonly Semaphore semaphore;
+        private List<int> players;
+
+        public Match(int copasity)
+        {
+            semaphore = new Semaphore(copasity, copasity);
+            players = new List<int>();
+            Console.WriteLine($"Матч на {copasity} игроков начат");
+        }
+
+        public void ConnectingPlayer(int clientId)
+        {
+            semaphore.WaitOne();
+
+            players.Add(clientId);
+            Console.WriteLine($"Игрок {clientId} подключился. Сейчас игроков в матче: {players.Count}");
+
+            Thread.Sleep(new Random().Next(20000, 40000));
+
+            if (!players.Contains(clientId))
+                return;
+
+            players.Remove(clientId);
+            Console.WriteLine($"Игрок {clientId} завершил сессию. Осталось игроков {players.Count}");
+
+            if (players.Count < 1)
+                Console.WriteLine("Матч завершен");
+
+            semaphore.Release();
+        }
+
+        public void CickPlayers(int clientId)
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine($"Игрок {clientId} был исключен за читы, тварюга такая");
+            players.Remove(clientId);
+
+            semaphore.Release();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using ThreadingExampleApp;
+
+namespace MutexTask
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            var match = new Match(copasity: 5);
+
+            for(int i = 1; i <= 10; i++)
+            {
+                int playerId = i;
+                new Thread(() =>
+                {
+                    match.ConnectingPlayer(playerId);
+                }).Start();
+
+                Thread.Sleep(new Random().Next(300, 1500));                
+            }
+
+            match.CickPlayers(5);
+        }
+    }
+}
